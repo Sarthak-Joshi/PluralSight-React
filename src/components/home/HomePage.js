dc4283c928da1783810be3d1990a1f821fae1c81
Sarthak Joshi <joshi.sarthak@gmail.com>
2016-10-19 12:53:40 -0400

    Added new message to Jumbotron (test commit)

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -10,7 +10,7 @@ class HomePage extends React.Component {
     render() {
         return (
             <div className="jumbotron">
-                <h1>Pluralsight Administration</h1>
+                <h1>Welcome to Pluralsight Administration</h1>
                 <p>React, React Router and Flux for ultra-responsive we apps.</p>
                 <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
             </div>
