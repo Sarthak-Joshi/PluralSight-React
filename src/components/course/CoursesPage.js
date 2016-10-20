d7571a1931dc5c00d0fd45741218314a66e08f72
Sarthak Joshi <joshi.sarthak@gmail.com>
2016-10-20 12:46:15 -0400

    Added empty form for Course Page

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class CoursesPage extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            course: { title: null }
+        };
+    }
+
+    onTitleChange(event) {
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState = ({ course: course });
+    }
     render() {
         return (
             <div>
                 <h1>Course</h1>
+                <h2>Add course</h2>
+                <input type="text" onChange={this.state.onTitleChange} value={this.state.course.title} />
+                <input type="text" value="Save" onClick={this.onClickSave} />
             </div>
         );
     }
