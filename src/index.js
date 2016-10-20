8fbb4d8e6805d6c01ca7a2f373d7d23b6d0aa8e2
Sarthak Joshi <joshi.sarthak@gmail.com>
2016-10-20 14:33:35 -0400

    Added react-redux provider to Index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-/* Webpack can install styles too..!!*/  
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
+/* Webpack can install styles too..!!*/
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-render (
-    <Router history={browserHistory} routes={routes}/>, document.getElementById('app')
+const store = configureStore(); // Can pass initialState
+
+render(
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>, document.getElementById('app')
 );
