aed6d564b97a1e1ba65b4777154c5ae333d9d1af
Sarthak Joshi <joshi.sarthak@gmail.com>
2016-10-20 13:37:51 -0400

    Form complete Added no console to eslint for debug Use of bind to make sure "this" into context

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -4,25 +4,44 @@ class CoursesPage extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            course: { title: null }
+            course: { title: "" }
         };
+
+        // onClick={this.onClickSave}
+        this.onClickSave = this.onClickSave.bind(this);
+        this.onTitleChange = this.onTitleChange.bind(this);
     }
 
     onTitleChange(event) {
+        console.log('Inside onTitleChange');
         const course = this.state.course;
         course.title = event.target.value;
-        this.setState = ({ course: course });
+        // this.setState() is function not varible
+        // Don't use equal to =  
+        this.setState({ course: course });
     }
+
+    onClickSave() {
+        alert(`Saving ${this.state.course.title}`);
+    }
+
     render() {
         return (
             <div>
                 <h1>Course</h1>
                 <h2>Add course</h2>
-                <input type="text" onChange={this.state.onTitleChange} value={this.state.course.title} />
-                <input type="text" value="Save" onClick={this.onClickSave} />
+                <input type="text"
+                    className="form-control"
+                    onChange={this.onTitleChange}
+                    value={this.state.course.title}
+                    />
+                <input type="submit"
+                    className="btn btn-sm btn-default"
+                    onClick={this.onClickSave} />
             </div>
         );
     }
 }
 
 export default CoursesPage;
+//  
